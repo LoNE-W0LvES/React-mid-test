44fd5d854f8a91a321e7c1d5768b34d05b553d7f
Nafi <nafimnr00@gmail.com>
2024-12-08 21:30:56 +0600

    React exam test project

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 
 import React from "react";
 import ProductList from "./components/ProductList";
-import Style from "./components/style.module.css";
 
 export default function ProductListPage() {
   return (
--- src/app/productList/page.jsx
+++ src/app/productList/page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ProductList from "../components/ProductList";
-import Style from "../components/style.module.css";
 
 export default function ProductListPage() {
     return (
